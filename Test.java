544874103fe7d635ec90cc942a2a4aa743a3fc67
Preet Kaler <preetsingh02064@gmail.com>
2026-03-11 13:32:51 +0530

    test

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i = 1; i < arr.length; i--){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        System.out.println("Prefix Sum implemented");
+    }
+}
